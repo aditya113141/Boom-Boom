371d184c3312f8045135dbab96919b2e85d4b48c
Adityakumar Sinha <adityakumar113141@gmail.com>
2021-05-28 12:50:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const score = document.querySelector('.score');
 
 const startScreen = document.querySelector('.startScreen');
 
-const gameArea= document.querySelector('.gameArea');
+ const gameArea= document.querySelector('.gameArea');
 
 startScreen.addEventListener('click',start);
 
